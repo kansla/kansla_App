eeb1110a941b4c65ec3bbee072a12f6a5f3a1604
ansuvin <50893537+ansuvin@users.noreply.github.com>
2020-12-28 21:37:48 +0900

    [CREATE] LoginActivity

---
+++ AndroidProject/app/src/main/java/com/example/androidproject/LoginActivity.java
@@ -0,0 +1,66 @@
+package com.example.androidproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    EditText editId, editPW;
+    TextView tvFind, tvSignup, errId, errPW;
+    Button btnLogin;
+
+    String strId, strPW;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        editId = findViewById(R.id.editId);
+        editPW = findViewById(R.id.editPW);
+        tvFind = findViewById(R.id.findIdPW);
+        tvSignup = findViewById(R.id.signup);
+        btnLogin = findViewById(R.id.loginBtn);
+        errId = findViewById(R.id.errId);
+        errPW = findViewById(R.id.errPW);
+
+        btnLogin.setOnClickListener(this);
+        tvFind.setOnClickListener(this);
+        tvSignup.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.findIdPW:
+                break;
+            case R.id.signup:
+                break;
+            case R.id.loginBtn:
+                strId = editId.getText().toString();
+                strPW = editPW.getText().toString();
+
+                if(strId.equals("") || strPW.equals("")) {
+                    if (strId.equals("")) {
+                        errId.setVisibility(View.VISIBLE);
+                    } else
+                        errId.setVisibility(View.GONE);
+                    if (strPW.equals("")) {
+                        errPW.setVisibility(View.VISIBLE);
+                    } else
+                        errPW.setVisibility(View.GONE);
+                }
+                else{
+                    errPW.setVisibility(View.GONE);
+                    errId.setVisibility(View.GONE);
+                    // 로그인 확인하기
+                }
+                break;
+        }
+    }
+}
