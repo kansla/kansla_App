299e6c7fe239e49624b8b11092929dcef35a9722
ansuvin <50893537+ansuvin@users.noreply.github.com>
2020-12-29 18:22:23 +0900

    [CREATE] SettingFragment

---
+++ AndroidProject/app/src/main/java/com/example/androidproject/ui/setting/SettingViewModel.java
@@ -0,0 +1,20 @@
+package com.example.androidproject.ui.setting;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SettingViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SettingViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
+
