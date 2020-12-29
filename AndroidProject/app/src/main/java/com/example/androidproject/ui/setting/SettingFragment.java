051c3c7fbee396174b8fa02b89fd09f18cab4969
ansuvin <50893537+ansuvin@users.noreply.github.com>
2020-12-29 18:37:00 +0900

    [CREATE] Infomation & Manual Activity

--- AndroidProject/app/src/main/java/com/example/androidproject/ui/setting/SettingFragment.java
+++ AndroidProject/app/src/main/java/com/example/androidproject/ui/setting/SettingFragment.java
@@ -1,21 +1,26 @@
 package com.example.androidproject.ui.setting;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.androidproject.MainActivity;
 import com.example.androidproject.R;
 
 
-public class SettingFragment extends Fragment {
+public class SettingFragment extends Fragment implements View.OnClickListener{
 
     View view;
+
+    ImageView btnInfo, btnManual;
 
     private SettingViewModel settingViewModel;
 
@@ -25,6 +30,28 @@ public class SettingFragment extends Fragment {
         settingViewModel =
                 new ViewModelProvider(this).get(SettingViewModel.class);
         view = inflater.inflate(R.layout.fragment_setting, container, false);
+
+        btnInfo = view.findViewById(R.id.info);
+        btnManual = view.findViewById(R.id.manual);
+
+        btnInfo.setOnClickListener(this);
+        btnManual.setOnClickListener(this);
+
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+        switch (v.getId()){
+            case R.id.info:
+                intent = new Intent(getActivity(), MainActivity.class);
+                break;
+            case R.id.manual:
+                intent = new Intent(getActivity(), MainActivity.class);
+                break;
+            default:
+        }
+        startActivity(intent);
     }
 }
