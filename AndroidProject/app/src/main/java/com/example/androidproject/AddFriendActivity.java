13b6c37c9531af5cbd14bac62aa8d72ce1cc1d34
ansuvin <dkstnqls0925@naver.com>
2021-01-06 15:18:52 +0900

    [UPDATE] Add Friend

--- AndroidProject/app/src/main/java/com/example/androidproject/AddFriendActivity.java
+++ AndroidProject/app/src/main/java/com/example/androidproject/AddFriendActivity.java
@@ -47,30 +47,35 @@ public class AddFriendActivity extends AppCompatActivity {
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                auto = getSharedPreferences("auto", Activity.MODE_PRIVATE);
-                AddFriend user = new AddFriend(auto.getString("inputId", ""), editId.getText().toString());
-                Call<AddFriend> call = RetrofitHelper.getApiService().friend_add(user);
-                call.enqueue(new Callback<AddFriend>() {
-                    @Override
-                    public void onResponse(Call<AddFriend> call, Response<AddFriend> response) {
-                        if(response.isSuccessful()){
-                            int result = response.code();
-                            if(result == 200) {
-                                textView.setText("친구 추가가 완료되었습니다.");
-                                textView.setVisibility(View.VISIBLE);
-                            }
-                            else if(result == 204){
-                                textView.setText("존재하지 않는 사용자입니다.");
-                                textView.setVisibility(View.VISIBLE);
+                if(editId.getText().toString().equals("")){
+                    textView.setText("친구 추가할 email을 입력해주세요");
+                    textView.setVisibility(View.VISIBLE);
+                }
+                else {
+                    auto = getSharedPreferences("auto", Activity.MODE_PRIVATE);
+                    AddFriend user = new AddFriend(auto.getString("inputId", ""), editId.getText().toString());
+                    Call<AddFriend> call = RetrofitHelper.getApiService().friend_add(user);
+                    call.enqueue(new Callback<AddFriend>() {
+                        @Override
+                        public void onResponse(Call<AddFriend> call, Response<AddFriend> response) {
+                            if (response.isSuccessful()) {
+                                int result = response.code();
+                                if (result == 200) {
+                                    textView.setText("친구 추가가 완료되었습니다.");
+                                    textView.setVisibility(View.VISIBLE);
+                                } else if (result == 204) {
+                                    textView.setText("존재하지 않는 사용자입니다.");
+                                    textView.setVisibility(View.VISIBLE);
+                                }
                             }
                         }
-                    }
 
-                    @Override
-                    public void onFailure(Call<AddFriend> call, Throwable t) {
-                        Log.e("err","통신 안됨");
-                    }
-                });
+                        @Override
+                        public void onFailure(Call<AddFriend> call, Throwable t) {
+                            Log.e("err", "통신 안됨");
+                        }
+                    });
+                }
             }
         });
 
